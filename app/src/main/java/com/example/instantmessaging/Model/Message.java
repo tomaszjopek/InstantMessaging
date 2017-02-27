c347ae55796dd6d863bfdbe2e88802101730429c
Tomasz Jopek <tomaszj@gmail.com>
2017-02-27 19:22:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/instantmessaging/Model/Message.java
@@ -0,0 +1,46 @@
+package com.example.instantmessaging.Model;
+
+import java.util.Date;
+
+/**
+ * Created by Tomek on 2017-02-27.
+ */
+
+public class Message {
+    private String message;
+    private String user;
+    private long time;
+
+    public Message(String message, String user, long time) {
+        this.message = message;
+        this.user = user;
+        this.time = new Date().getTime();
+    }
+
+    public Message() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
